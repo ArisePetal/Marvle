7f13821e950d930b7953c4f5947c04064b42c841
ArisePetal <105610420+ArisePetal@users.noreply.github.com>
2025-04-08 20:42:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('next-clue-btn').addEventListener('click', function() {
+    const clue = document.getElementById('clue');
+    clue.innerHTML = "Iron Man is a genius inventor!";
+});
